501542f2c32ab733826e56740cddfb51355a2cf3
Matt Trice <matttrice@pm.me>
2025-01-02 17:54:07 -0700

    setup for potentially storing and reusing auth context

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, Browser, BrowserContext
 from dotenv import load_dotenv
 
 from config import get_header
@@ -14,14 +14,34 @@ def browser_context_args():
     }
 
 
-# Module scope to store login context for all tests at beginning of run
-# Use function scope to login for each test
-@pytest.fixture(scope="module", autouse=True)
-def login_context(page: Page):
-     page.goto(os.getenv("TEST_LOCALE")+"/sign-in")
+@pytest.fixture(scope="module")
+def auth_context(browser: Browser) -> BrowserContext:
+    context = browser.new_context(
+        base_url=os.getenv("TEST_BASE_URL")
+    )
+    page = context.new_page()
     
-     page.fill('input[name="username"]', os.getenv("TEST_USERNAME"))
-     page.fill('input[name="password"]', os.getenv("TEST_PASSWORD"))
-     page.click('button[type="submit"]')
+    page.goto(os.getenv("TEST_LOCALE")+"/sign-in")
+
+    page.fill('input[name="username"]', os.getenv("TEST_USERNAME"))
+    page.fill('input[name="password"]', os.getenv("TEST_PASSWORD"))
+    page.click('button[type="submit"]')
     
-     expect(page.get_by_role("button", name=get_header["buy_crypto"])).to_be_visible()
+    # Validate authenticated page
+    expect(page.get_by_role("button", name=get_header("buy_crypto"))).to_be_visible()
+    
+    storage_state = context.storage_state()
+    
+    authenticated_context = browser.new_context(
+        storage_state=storage_state,
+        base_url=os.getenv("TEST_BASE_URL")
+    )
+    
+    yield authenticated_context
+    authenticated_context.close()
+
+@pytest.fixture
+def page(auth_context: BrowserContext) -> Page:
+    page = auth_context.new_page()
+    yield page
+    page.close()
