e95a3cf26c0c3433cdfe268d1b5242556e5c947d
Matt Trice <matttrice@pm.me>
2025-01-02 17:54:27 -0700

    rename test so pytest finds it

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,9 +1,9 @@
+import pytest
 from playwright.sync_api import Page, expect
 
 from config import get_url
 
-
-def test_user_can_view_portfolio(page: Page):
+def test_user_can_view_portfolio(page: pytest.fixture):
     page.goto(get_url("portfolio"))
     
     expect(page.get_by_text("Portfolio value")).to_be_visible()
