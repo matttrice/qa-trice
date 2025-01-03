bd0da807c04525872450d9fbe3b191778eb29702
Matt Trice <matttrice@pm.me>
2025-01-02 22:02:01 -0700

    add value check

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,5 +1,6 @@
 import pytest
-from playwright.sync_api import Page, expect
+import os
+from playwright.sync_api import expect
 
 from config import get_url
 
@@ -7,3 +8,4 @@ def test_user_can_view_portfolio(page: pytest.fixture):
     page.goto(get_url("portfolio"))
     
     expect(page.get_by_text("Portfolio value")).to_be_visible()
+    expect(page.get_by_text(os.getenv("TEST_PORTFOLIO_VALUE"), exact=True)).to_be_visible()
